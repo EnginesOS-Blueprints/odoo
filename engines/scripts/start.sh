09d9b56af9e648e20f31e12afbd1d64fb7f7880e
James Vodanovich <james@jvodan.com>
2015-01-25 00:58:43 +1100

    first commit

--- engines/scripts/start.sh
+++ engines/scripts/start.sh
@@ -1,3 +1,2 @@
 cd /home/app
 ./openerp-gevent -c debian/openerp-server.conf  -d $dbname -r $dbuser  -w $dbpasswd --db_host=$dbhost --without-demo="all"  -D $VOLDIR/   --logfile=/home/app/log  --logrotate --smtp=smtp.engines.internal --email-from=$from_email  --timezone=$TZ
-
